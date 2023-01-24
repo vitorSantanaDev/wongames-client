df9ac06765b5d5e20506dfaea41dfdda7054703a
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-01-23 21:23:51 -0300

    test: creating snapshot for the Heading component

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -7,10 +7,12 @@ import Heading from '.'
 
 describe('<Heading />', () => {
   it('should render a white heading by default', () => {
-    renderWithTheme(<Heading>Won Games</Heading>)
+    const { container } = renderWithTheme(<Heading>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       color: theme.colors.white
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render a black heading when color is passed', () => {
@@ -29,5 +31,13 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  it('should render a heading with a line at the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'padding-left': `${theme.spacings.xxsmall}`,
+      'border-left': ` 0.7rem solid ${theme.colors.secondary}`
+    })
   })
 })
