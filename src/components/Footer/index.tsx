eb74dc2c3d757a36dc698086e152788e5106cd5b
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-01-25 21:18:45 -0300

    feat: creating Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,71 @@
+import * as S from './styles'
+
+import Logo from 'components/Logo'
+import Heading from 'components/Heading'
+import Link from 'next/link'
+
+const Footer = () => (
+  <S.Wrapper>
+    <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" lineBottom lineColor="secondary" size="small">
+          Contact
+        </Heading>
+        <a href="mailto:@vitorsantana.developer@gmail.com">example@gmail.com</a>
+        <a href="#" target="_blank" rel="noopenner, noreferrer">
+          9 0099-0099
+        </a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineBottom lineColor="secondary" size="small">
+          Follow us
+        </Heading>
+        <nav aria-labelledby="social media">
+          <a href="#" target="_blank" rel="noopenner, noreferrer">
+            Instagram
+          </a>
+          <a href="#" target="_blank" rel="noopenner, noreferrer">
+            Twitter
+          </a>
+          <a href="#" target="_blank" rel="noopenner, noreferrer">
+            Youtube
+          </a>
+          <a href="#" target="_blank" rel="noopenner, noreferrer">
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineBottom lineColor="secondary" size="small">
+          Links
+        </Heading>
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Buscar</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column aria-labelledby="footer contact">
+        <Heading color="black" lineBottom lineColor="secondary" size="small">
+          Location
+        </Heading>
+        <span>Rua 7 de Maio</span>
+        <span>527 - 89020330</span>
+        <span>Rio de Janeiro, Brasil</span>
+      </S.Column>
+    </S.Content>
+    <S.Copyright>Won Games 2020 © Todos os Direitos Reservados</S.Copyright>
+  </S.Wrapper>
+)
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,50 @@
+import styled, { css } from 'styled-components'
+import * as HeadingStyles from 'components/Heading/styles'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.footer`
+  ${() => css`
+    ${HeadingStyles.Wrapper} {
+      text-transform: uppercase;
+    }
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    gap: ${theme.grid.gutter};
+    grid-template-columns: repeat(2, 1fr);
+    margin-top: ${theme.spacings.medium};
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: repeat(4, 1fr);
+    `}
+  `}
+`
+
+export const Column = styled.div`
+  ${({ theme }) => css`
+    a,
+    span {
+      display: block;
+      text-decoration: none;
+      color: ${theme.colors.gray};
+      font-size: ${theme.font.sizes.small};
+      margin-bottom: ${theme.spacings.xxsmall};
+    }
+    a:hover {
+      text-decoration: underline;
+    }
+  `}
+`
+
+export const Copyright = styled.div`
+  ${({ theme }) => css`
+    text-align: center;
+    color: ${theme.colors.gray};
+    margin-top: ${theme.spacings.large};
+    font-size: ${theme.font.sizes.xsmall};
+    margin-bottom: ${theme.spacings.medium};
+  `}
+`
