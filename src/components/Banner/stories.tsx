15cebba85f7c46554154146b722f8598277a821a
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-01-27 20:22:11 -0300

    feat: creating Banner component

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react'
+import Banner from '.'
+import { BannerProps } from './types'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<BannerProps> = (args) => <Banner {...args} />
---
+++ src/components/Banner/types.ts
@@ -0,0 +1,7 @@
+export type BannerProps = {
+  img: string
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
