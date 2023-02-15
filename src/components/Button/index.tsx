334f70e8f5222d1f6f05946f4e72c8e973c77b18
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-02-14 21:40:12 -0300

    refactor: wrapping Button component in ForwardRef to be able to receive references

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,25 +1,31 @@
+import React from 'react'
 import * as S from './styles'
 
 import { ButtonProps } from './types'
 
-const Button = ({
-  icon,
-  children,
-  size = 'medium',
-  minimal = false,
-  fullWidth = false,
-  ...props
-}: ButtonProps) => (
-  <S.Wrapper
-    size={size}
-    hasIcon={!!icon}
-    minimal={minimal}
-    fullWidth={fullWidth}
-    {...props}
-  >
-    {!!icon && icon}
-    {!!children && <span>{children}</span>}
-  </S.Wrapper>
-)
-
-export default Button
+const Button: React.ForwardRefRenderFunction<S.WrapperProps, ButtonProps> = (
+  {
+    icon,
+    children,
+    size = 'medium',
+    minimal = false,
+    fullWidth = false,
+    ...props
+  },
+  ref
+) => {
+  return (
+    <S.Wrapper
+      size={size}
+      hasIcon={!!icon}
+      minimal={minimal}
+      fullWidth={fullWidth}
+      ref={ref}
+      {...props}
+    >
+      {!!icon && icon}
+      {!!children && <span>{children}</span>}
+    </S.Wrapper>
+  )
+}
+export default React.forwardRef(Button)
