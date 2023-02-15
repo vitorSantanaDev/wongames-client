73092e20a662ac706ee1a0430c5b1e2e40c546ef
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-02-15 09:38:13 -0300

    test: ignoring unnecessary files in test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/types.ts',
+    '!src/**/mock.ts',
+    '!src/**/stories.tsx',
+    '!src/pages/*.ts(x)',
+    '!src/styles/*.ts(x)'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
   transform: {
