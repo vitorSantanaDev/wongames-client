5e75812e866746d4654a8d5df21cbfd88a231e78
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-02-17 09:28:59 -0300

    refactor: wrapping component in a forwardRef

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,13 +1,19 @@
+import { forwardRef } from 'react'
 import SlickSlider from 'react-slick'
 
 import { SliderProps } from './types'
 
 import * as S from './styles'
 
-const Slider = ({ children, settings }: SliderProps) => (
+const Slider: React.ForwardRefRenderFunction<SlickSlider, SliderProps> = (
+  { children, settings },
+  ref
+) => (
   <S.Wrapper>
-    <SlickSlider {...settings}>{children}</SlickSlider>
+    <SlickSlider ref={ref} {...settings}>
+      {children}
+    </SlickSlider>
   </S.Wrapper>
 )
 
-export default Slider
+export default forwardRef(Slider)
