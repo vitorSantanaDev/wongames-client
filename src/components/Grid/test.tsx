76c753e36f82a83a78af999c1177c0296bd26b94
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-02-19 20:34:57 -0300

    feat: creating Grid component

---
+++ src/components/Grid/test.tsx
@@ -0,0 +1,34 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Grid } from '.'
+
+describe('<Grid />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(
+      <Grid>
+        <h1>Children 1</h1>
+        <h1>Children 2</h1>
+      </Grid>
+    )
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        display: grid;
+        grid-gap: 3.2rem;
+        margin: 3.2rem 0;
+        grid-template-columns: repeat(auto-fill,minmax(25rem,1fr));
+      }
+
+      <div
+        class="c0"
+      >
+        <h1>
+          Children 1
+        </h1>
+        <h1>
+          Children 2
+        </h1>
+      </div>
+    `)
+  })
+})
