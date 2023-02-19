75157643271be78c50288f63743a303f5c7ee4d9
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-02-19 15:17:00 -0300

    style: updating style

--- src/components/Container/test.tsx
+++ src/components/Container/test.tsx
@@ -19,6 +19,7 @@ describe('<Container />', () => {
 
     expect(container.firstChild).toMatchInlineSnapshot(`
       .c0 {
+        width: 100%;
         margin-left: auto;
         margin-right: auto;
         padding-left: calc(3.2rem / 2);
