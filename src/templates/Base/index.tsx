f58f98f47845709927fab1c66fd198acdb1dc549
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-02-19 15:16:22 -0300

    refactor: improvements

--- src/templates/Base/index.tsx
+++ src/templates/Base/index.tsx
@@ -11,7 +11,7 @@ const Base = ({ children }: BaseProps) => (
     <Container>
       <Menu />
     </Container>
-    {children}
+    <S.Content>{children}</S.Content>
     <S.SectionFooter>
       <Container>
         <Footer />
--- src/templates/Base/styles.ts
+++ src/templates/Base/styles.ts
@@ -1,7 +1,16 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
-export const Wrapper = styled.section``
+export const Wrapper = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+export const Content = styled.div`
+  flex: 1 0 auto;
+`
 
 export const SectionFooter = styled.section`
   ${({ theme }) => css`
