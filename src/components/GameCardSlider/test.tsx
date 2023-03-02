e3985356bb285ba21f0a397be8cf6cdb383081df
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-03-02 20:52:19 -0300

    fix: correcting mock types

--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -3,44 +3,11 @@ import 'match-media-mock'
 import GameCardSlider from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-const items = [
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    image: 'https://wallpaperaccess.com/full/625497.jpg',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  },
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    image: 'https://wallpaper.dog/large/10801367.jpg',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  },
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    image: 'https://images4.alphacoders.com/937/937844.png',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  },
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    image: 'https://wallpaper.dog/large/10800384.jpg',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  }
-]
+import itemsMock from './mock'
 
 describe('<GameCardSlider />', () => {
   it('should render with 4 active items', () => {
-    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+    const { container } = renderWithTheme(<GameCardSlider items={itemsMock} />)
 
     expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
 
