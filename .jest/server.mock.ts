b211de5b28b3ac5303b2496f6cf7b4bffa633b8c
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-04-03 21:23:41 -0300

    chore: preparing jet to work with interceptions for api

---
+++ .jest/server.mock.ts
@@ -0,0 +1,9 @@
+global.fetch = require('node-fetch')
+
+import { server } from '../src/utils/mockServer/server'
+
+beforeAll(() => server.listen())
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
