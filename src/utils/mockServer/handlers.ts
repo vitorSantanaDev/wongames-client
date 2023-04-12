db813a5d9bb961c2cad425c3011997e3f046ee0d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-04-12 09:24:39 -0300

    test: creating form reset password tests

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -2,6 +2,12 @@ import { rest } from 'msw'
 
 type LoginRequestBody = { email: string }
 
+type ResetPasswordRequestBody = {
+  code: string
+  password: string
+  passwordConfirmation: string
+}
+
 export const handlers = [
   rest.post<LoginRequestBody>(
     `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
@@ -18,6 +24,27 @@ export const handlers = [
         )
       }
       return res(ctx.status(200), ctx.json({ ok: true }))
+    }
+  ),
+  rest.post<ResetPasswordRequestBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+    (req, res, ctx) => {
+      const { code } = req.body
+
+      if (code === 'wrong_code') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [{ messages: [{ message: 'Incorrect code provided.' }] }]
+          })
+        )
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({ user: { email: 'valid@email.com' } })
+      )
     }
   )
 ]
