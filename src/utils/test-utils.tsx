09f4d3e14dc2758e6c2f2ba87190f4a24d164e98
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-04-17 08:27:59 -0300

    refactor: adding the wishlist provider in the test utils file

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,25 +1,34 @@
 import { render, RenderOptions } from '@testing-library/react'
 import { CartContext, CartContextDefaultValues } from 'hooks/use-cart'
 import { CartContextData } from 'hooks/use-cart/types'
+import {
+  WishlistContext,
+  WishlistContextDefaultValues
+} from 'hooks/use-wishlist'
+import { WishlistContextData } from 'hooks/use-wishlist/types'
 import { ReactElement } from 'react'
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 
 type CustomRenderProps = {
   cartProviderProps?: CartContextData
+  wishlistProviderProps?: WishlistContextData
 } & Omit<RenderOptions, 'queries'>
 
 const customRender = (
   ui: ReactElement,
   {
     cartProviderProps = CartContextDefaultValues,
+    wishlistProviderProps = WishlistContextDefaultValues,
     ...renderOptions
   }: CustomRenderProps = {}
 ) =>
   render(
     <ThemeProvider theme={theme}>
       <CartContext.Provider value={cartProviderProps}>
-        {ui}
+        <WishlistContext.Provider value={wishlistProviderProps}>
+          {ui}
+        </WishlistContext.Provider>
       </CartContext.Provider>
     </ThemeProvider>,
     renderOptions
